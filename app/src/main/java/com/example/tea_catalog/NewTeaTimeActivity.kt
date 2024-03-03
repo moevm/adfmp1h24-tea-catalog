b6eb7011ba28d0dbd6f48146159aaf92852ff7ae
AfanasiefDV <dima086AF@yandex.ru>
2024-03-03 17:54:24 +0300

    finish tea time

---
+++ app/src/main/java/com/example/tea_catalog/NewTeaTimeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tea_catalog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewTeaTimeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_tea_time)
+    }
+}
