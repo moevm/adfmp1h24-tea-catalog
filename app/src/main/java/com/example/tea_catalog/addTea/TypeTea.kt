d4e9337497d6173d52a11dcbac493061179fb091
MihailPereverza <mihailpereverza@yandex.ru>
2024-03-04 22:55:40 +0300

    temp

--- app/src/main/java/com/example/tea_catalog/addTea/TypeTea.kt
+++ app/src/main/java/com/example/tea_catalog/addTea/TypeTea.kt
@@ -35,7 +35,7 @@ class TypeTea : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_type_tea, container, false)
+        return inflater.inflate(R.layout.fragment_type_tea__radio, container, false)
     }
 
     companion object {
