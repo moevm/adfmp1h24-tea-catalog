e8322de7f8301868e40b80d0fb8cec7ee06447ef
cadbee <aschiam.net@gmail.com>
2024-03-04 22:29:03 +0300

    навигация

--- app/src/main/java/com/example/tea_catalog/HomeScreenActivity.kt
+++ app/src/main/java/com/example/tea_catalog/HomeScreenActivity.kt
@@ -2,10 +2,17 @@ package com.example.tea_catalog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class HomeScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.home)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 }
--- app/src/main/java/com/example/tea_catalog/MainActivity.kt
+++ app/src/main/java/com/example/tea_catalog/MainActivity.kt
@@ -1,16 +1,38 @@
 package com.example.tea_catalog
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        var homeBtn = findViewById<ImageButton>(R.id.homeBtn);
+        var likedBtn = findViewById<ImageButton>(R.id.likedBtn);
+        var teatimeBtn = findViewById<ImageButton>(R.id.teatimeBtn);
+
+        homeBtn.setOnClickListener {
+            val intent = Intent(this@MainActivity, HomeScreenActivity::class.java)
+            startActivity(intent)
+        }
+
+        likedBtn.setOnClickListener {
+            val intent = Intent(this@MainActivity, FavoriteTeaListActivity::class.java)
+            startActivity(intent)
+        }
+
+        teatimeBtn.setOnClickListener {
+            val intent = Intent(this@MainActivity, DiaryActivity::class.java)
+            startActivity(intent)
+        }
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
