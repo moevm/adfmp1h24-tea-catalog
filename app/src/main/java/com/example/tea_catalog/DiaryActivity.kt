99a1e32b76c077b41c749cb6f0fc64d8e4895686
cadbee <aschiam.net@gmail.com>
2024-03-04 22:53:47 +0300

    основные экраны

--- app/src/main/java/com/example/tea_catalog/DiaryActivity.kt
+++ app/src/main/java/com/example/tea_catalog/DiaryActivity.kt
@@ -1,11 +1,27 @@
 package com.example.tea_catalog
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class DiaryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_diary)
+
+        var homeBtn = findViewById<ImageButton>(R.id.homeBtn);
+        var likedBtn = findViewById<ImageButton>(R.id.likedBtn);
+
+        homeBtn.setOnClickListener {
+            val intent = Intent(this@DiaryActivity, HomeScreenActivity::class.java)
+            startActivity(intent)
+        }
+
+        likedBtn.setOnClickListener {
+            val intent = Intent(this@DiaryActivity, FavoriteTeaListActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/example/tea_catalog/HomeScreenActivity.kt
+++ app/src/main/java/com/example/tea_catalog/HomeScreenActivity.kt
@@ -1,7 +1,9 @@
 package com.example.tea_catalog
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -9,6 +11,20 @@ class HomeScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_screen)
+
+        var likedBtn = findViewById<ImageButton>(R.id.likedBtn);
+        var teatimeBtn = findViewById<ImageButton>(R.id.teatimeBtn);
+
+        likedBtn.setOnClickListener {
+            val intent = Intent(this@HomeScreenActivity, FavoriteTeaListActivity::class.java)
+            startActivity(intent)
+        }
+
+        teatimeBtn.setOnClickListener {
+            val intent = Intent(this@HomeScreenActivity, DiaryActivity::class.java)
+            startActivity(intent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.home)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
