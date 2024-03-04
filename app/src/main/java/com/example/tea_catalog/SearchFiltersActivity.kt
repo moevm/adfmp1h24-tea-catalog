c7187c1e629ec5743aa32f9e8cef9884cc279369
MihailPereverza <mihailpereverza@yandex.ru>
2024-03-05 00:02:16 +0300

    temp 7.0

--- app/src/main/java/com/example/tea_catalog/SearchFiltersActivity.kt
+++ app/src/main/java/com/example/tea_catalog/SearchFiltersActivity.kt
@@ -1,6 +1,9 @@
 package com.example.tea_catalog
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +14,32 @@ class SearchFiltersActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_search_filters)
+
+        var homeBtn = findViewById<ImageButton>(R.id.homeBtn);
+        var likedBtn = findViewById<ImageButton>(R.id.likedBtn);
+        var teatimeBtn = findViewById<ImageButton>(R.id.teatimeBtn);
+        var backBtn = findViewById<ImageView>(R.id.searchFiltersBackBtn);
+
+        homeBtn.setOnClickListener {
+            val intent = Intent(this@SearchFiltersActivity, HomeScreenActivity::class.java)
+            startActivity(intent)
+        }
+
+        likedBtn.setOnClickListener {
+            val intent = Intent(this@SearchFiltersActivity, FavoriteTeaListActivity::class.java)
+            startActivity(intent)
+        }
+
+        teatimeBtn.setOnClickListener {
+            val intent = Intent(this@SearchFiltersActivity, DiaryActivity::class.java)
+            startActivity(intent)
+        }
+
+        backBtn.setOnClickListener {
+            val intent = Intent(this@SearchFiltersActivity, FavoriteTeaListActivity::class.java)
+            startActivity(intent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
