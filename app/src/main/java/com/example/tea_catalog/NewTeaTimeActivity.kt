6a51f1897dde6e94a3c3efeeceb74a693be347bd
AfanasiefDV <dima086AF@yandex.ru>
2024-03-04 22:51:13 +0300

    add on click

--- app/src/main/java/com/example/tea_catalog/NewTeaTimeActivity.kt
+++ app/src/main/java/com/example/tea_catalog/NewTeaTimeActivity.kt
@@ -1,11 +1,38 @@
 package com.example.tea_catalog
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 
 class NewTeaTimeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_tea_time)
+        var homeBtn = findViewById<ImageButton>(R.id.homeBtn);
+        var likedBtn = findViewById<ImageButton>(R.id.likedBtn);
+        var teatimeBtn = findViewById<ImageButton>(R.id.teatimeBtn);
+
+        var back = findViewById<Button>(R.id.imageButton1Back);
+        homeBtn.setOnClickListener {
+            val intent = Intent(this@NewTeaTimeActivity, HomeScreenActivity::class.java)
+            startActivity(intent)
+        }
+
+        likedBtn.setOnClickListener {
+            val intent = Intent(this@NewTeaTimeActivity, FavoriteTeaListActivity::class.java)
+            startActivity(intent)
+        }
+
+        teatimeBtn.setOnClickListener {
+            val intent = Intent(this@NewTeaTimeActivity, DiaryActivity::class.java)
+            startActivity(intent)
+        }
+        back.setOnClickListener {
+            val intent = Intent(this@NewTeaTimeActivity, DiaryActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
